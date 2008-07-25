b1c76c2f05edcfcfac007b9460ae20061cb6dc43
Dustin Sallings <dustin@spy.net>
2008-07-25 09:34:40 -0700

    Added a query class and test.

---
+++ atombot/query.rb
@@ -0,0 +1,37 @@
+require 'set'
+
+module AtomBot
+
+  DASH='-'[0]
+
+  class QueryParseException < StandardError
+  end
+
+  class Query
+
+    attr_reader :positive, :negative
+
+    def initialize(query)
+      positive, negative = words = query.split.uniq.partition { |w| w[0] != DASH }
+      @positive = positive.map {|w| w.to_sym}
+      @negative = negative.map {|w| w.sub(/^-/, '').to_sym}
+
+      if @positive.empty?
+        raise QueryParseException.new("You must supply at least one positive match")
+      end
+      unless Set.new(@positive).intersection(@negative).empty?
+        raise QueryParseException.new("You can't have a positive and negative match for the same term.")
+      end
+    end
+
+    def matches?(text)
+      words = Set.new(text.gsub(/[.,'";]/, '').downcase.split)
+      pos_str = Set.new(@positive.map{|w| w.to_s})
+      neg_str = Set.new(@negative.map{|w| w.to_s})
+
+      pos_str.proper_subset?(words) && neg_str.intersection(words).empty?
+    end
+
+  end
+
+end
---
+++ test/query_test.rb
@@ -0,0 +1,74 @@
+require "test/unit"
+
+require "../atombot/query"
+
+class QueryTest < Test::Unit::TestCase
+
+  include AtomBot
+
+  def test_single_construct
+    q = Query.new 'aword'
+    assert_equal [:aword], q.positive
+    assert_equal [], q.negative
+  end
+
+  def test_multiple_construct
+    q = Query.new 'aword anotherword'
+    assert_equal [:aword, :anotherword], q.positive
+    assert_equal [], q.negative
+  end
+
+  def test_duplicate_construct
+    q = Query.new 'aword anotherword aword'
+    assert_equal [:aword, :anotherword], q.positive
+    assert_equal [], q.negative
+  end
+
+  def test_empty_construct
+    assert_raise(QueryParseException) { q = Query.new '' }
+  end
+
+  def test_negative_only_construct
+    assert_raise(QueryParseException) { q = Query.new '-junk' }
+  end
+
+  def test_positive_and_negative_construct
+    q = Query.new 'aword anotherword -badword -otherbadword'
+    assert_equal [:aword, :anotherword], q.positive
+    assert_equal [:badword, :otherbadword], q.negative
+  end
+
+  def test_positive_and_negative_overlap_construct
+    assert_raise(QueryParseException) { q = Query.new 'something -something' }
+  end
+
+  # Match tests
+
+  def test_simple_postive_match
+    q = Query.new 'word'
+    assert q.matches?('This thing contains a word.')
+  end
+
+  def test_simple_negative_match
+    q = Query.new 'word'
+    assert !q.matches?('But this one does not.')
+  end
+
+  def test_multi_positive_match
+    q = Query.new 'cool stuff'
+    assert q.matches?("This is about some really cool stuff.")
+    assert q.matches?("Cool and stuff don't need to be next to each other.")
+
+    assert !q.matches?("This one is just cool.")
+    assert !q.matches?("And this one just has stuff.")
+  end
+
+  def test_multi_positive_and_negative_matches
+    q = Query.new 'cool stuff -paint -brush'
+    assert q.matches?("Stuff that's cool matches.")
+    assert !q.matches?("Is a paint brush cool stuff?")
+    assert !q.matches?("Is a brush cool stuff?")
+    assert !q.matches?("Is paint cool stuff?")
+  end
+
+end
