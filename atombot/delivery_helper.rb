e1f2bf790a9e84910ce88b2cdf2255ed16adcc3a
Dustin Sallings <dustin@spy.net>
2008-07-31 09:02:57 -0700

    Allow User objects to be sent to deliver.

--- atombot/delivery_helper.rb
+++ atombot/delivery_helper.rb
@@ -1,21 +1,31 @@
 module AtomBot
   module DeliveryHelper
 
-    def deliver(jid, message, type=:chat)
-      u = User.first(:jid => jid)
-      if u.nil? || !u.ready_to_receive_message
-        puts "... Suppressing send to #{jid}"
-      else
-        deliver_without_suppression(jid, message, type)
+    def resolve_user(recip)
+      case recip
+        when String
+          User.first(:jid => recip)
+        when User
+          recip
       end
     end
 
-    def deliver_without_suppression(jid, message, type=:chat)
+    def deliver(recip, message, type=:chat)
+      u = resolve_user(recip)
+      if u.nil? || !u.ready_to_receive_message
+        puts "... Suppressing send to #{jid}"
+      else
+        deliver_without_suppression(u, message, type)
+      end
+    end
+
+    def deliver_without_suppression(recip, message, type=:chat)
+      u = resolve_user(recip)
       if message.kind_of?(Jabber::Message)
         msg = message
-        msg.to = jid
+        msg.to = u.jid
       else
-        msg = Jabber::Message.new(jid)
+        msg = Jabber::Message.new(u.jid)
         msg.type = type
         msg.body = message
       end
