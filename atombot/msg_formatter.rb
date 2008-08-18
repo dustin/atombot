bff9e43983ab0b2fd5f6bdb703c3fba04a36f065
Dustin Sallings <dustin@spy.net>
2008-08-18 01:46:45 -0700

    Fix service recognition.

--- atombot/msg_formatter.rb
+++ atombot/msg_formatter.rb
@@ -2,17 +2,18 @@ module AtomBot
 
   module MsgFormatter
 
-    def user_link(user, service)
+    def user_link(user, svc)
       linktext=user
       if user[0] == $@ # Does it start with @?
         user = user.gsub(/^@(.*)/, '\1')
       end
-      case service
+      case svc
       when 'twitter'
         %Q{<a href="http://twitter.com/#{user}">#{linktext}</a>}
       when 'identica'
         %Q{<a href="http://identi.ca/#{user}">#{linktext}</a>}
       else
+        puts "Unknown service for providing a user link:  #{svc}"
         linktext
       end
     end
@@ -21,9 +22,9 @@ module AtomBot
       type.nil? ? '' : "[#{type}] "
     end
 
-    def format_html_body(from, text, type, service)
-      user = user_link(from, service)
-      text = text.gsub(/(\W*)(@[\w_]+)/) {|x| $1 + user_link($2)}.gsub(/&/, '&amp;')
+    def format_html_body(from, text, type, svc)
+      user = user_link(from, svc)
+      text = text.gsub(/(\W*)(@[\w_]+)/) {|x| $1 + user_link($2, svc)}.gsub(/&/, '&amp;')
       "#{type_str(type)}#{user}: #{text}"
     end
 
@@ -31,12 +32,12 @@ module AtomBot
       "#{type_str(type)}#{from}: #{text}"
     end
 
-    def format_msg(service, jid, from, text, subject="Track Message", type=nil)
+    def format_msg(svc, jid, from, text, subject="Track Message", type=nil)
       body = format_plain_body(from, text, type)
       m = Jabber::Message::new(jid, body).set_type(:chat).set_id('1').set_subject(subject)
 
       # The html itself
-      html = format_html_body(from, text, type, service)
+      html = format_html_body(from, text, type, svc)
       begin
         REXML::Document.new "<html>#{html}</html>"
 
@@ -61,7 +62,7 @@ module AtomBot
     end
 
     def format_track_msg(stuff)
-      format_msg(stuff[:service], stuff[:to], stuff[:author], stuff[:message])
+      format_msg(stuff[:source], stuff[:to], stuff[:author], stuff[:message])
     end
   end
 
