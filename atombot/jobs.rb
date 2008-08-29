92e99824169304990fbb96bf89ec4e742bf81d10
Dustin Sallings <dustin@spy.net>
2008-08-28 21:43:12 -0700

    Deduplicate jobs.

--- atombot/jobs.rb
+++ atombot/jobs.rb
@@ -29,8 +29,22 @@ module AtomBot
       MultiMatch.recache_all
     end
 
-    def process
-      job = @beanstalk.reserve
+    def get_deduplicated_jobs
+      rv = {}
+      loop do
+        job = @beanstalk.reserve(0)
+        if rv.keys.include? job.ybody
+          puts "Removing duplicate job:  #{job.ybody[:type]}"
+          job.delete
+        else
+          rv[job.ybody] = job
+        end
+      end
+    rescue Beanstalk::TimedOut
+      rv.values
+    end
+
+    def run_job(job)
       stuff = job.ybody
       timing = Benchmark.measure do
         self.send "process_#{stuff[:type]}", stuff
@@ -44,6 +58,10 @@ module AtomBot
     ensure
       job.decay unless job.nil?
       $stdout.flush
+    end
+
+    def process
+      get_deduplicated_jobs.each { |job| run_job job }
     end
 
     def run
