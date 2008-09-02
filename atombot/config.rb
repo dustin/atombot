493fcf029a2cb8ec25d8dac4fbf698fe67c1379c
Dustin Sallings <dustin@spy.net>
2008-09-01 21:58:22 -0700

    Remove last sign of feeders.

--- atombot/config.rb
+++ atombot/config.rb
@@ -8,7 +8,6 @@ require 'atombot/logging'
 module AtomBot
   module Config
     CONF = ::YAML.load_file 'atombot.yml'
-    FEEDERS = CONF['feeders']
     VERSION = `git rev-parse --short HEAD`
     SCREEN_NAME = CONF['incoming']['jid']
     IGNORED_JIDS = CONF['ignored'] || []
