6d0be7693c27fd310c4889a3adf4c0d6196b7e8f
Dustin Sallings <dustin@spy.net>
2008-10-07 08:58:43 -0700

    Run all of the match tests against the raw object and a marshaled object.

--- test/multimatch_test.rb
+++ test/multimatch_test.rb
@@ -7,33 +7,39 @@ class QueryTest < Test::Unit::TestCase
   include AtomBot
 
   def test_multi_match_no_input
-    mm = MultiMatch.new []
-    assert_equal Set.new, mm.matches(%w(some text that can't match))
+    run_test(MultiMatch.new([])) do |mm|
+      assert_equal Set.new, mm.matches(%w(some text that can't match))
+    end
   end
 
   def test_multi_match_one_input_miss
-    mm = MultiMatch.new [['key', 1]]
-    assert_equal Set.new, mm.matches(%w(some text that can't match))
+    run_test(MultiMatch.new([['key', 1]])) do |mm|
+      assert_equal Set.new, mm.matches(%w(some text that can't match))
+    end
   end
 
   def test_multi_match_one_input_hit
-    mm = MultiMatch.new [['match', 1]]
-    assert_equal Set.new([1]), mm.matches(%w(some text that does match))
+    run_test(MultiMatch.new([['match', 1]])) do |mm|
+      assert_equal Set.new([1]), mm.matches(%w(some text that does match))
+    end
   end
 
   def test_multi_match_multi_hit_one_result
-    mm = MultiMatch.new [['match', 1], ['does', 1]]
-    assert_equal Set.new([1]), mm.matches(%w(some text that does match))
+    run_test(MultiMatch.new([['match', 1], ['does', 1]])) do |mm|
+      assert_equal Set.new([1]), mm.matches(%w(some text that does match))
+    end
   end
 
   def test_multi_match_multi_hit_multi_results
-    mm = MultiMatch.new [['match', 1], ['does', 2]]
-    assert_equal Set.new([1, 2]), mm.matches(%w(some text that does match))
+    run_test(MultiMatch.new([['match', 1], ['does', 2]])) do |mm|
+      assert_equal Set.new([1, 2]), mm.matches(%w(some text that does match))
+    end
   end
 
   def test_multi_match_multi_hit_single_word_multi_reuslts
-    mm = MultiMatch.new [['match', 1], ['match', 2]]
-    assert_equal Set.new([1, 2]), mm.matches(%w(some text that does match))
+    run_test(MultiMatch.new([['match', 1], ['match', 2]])) do |mm|
+      assert_equal Set.new([1, 2]), mm.matches(%w(some text that does match))
+    end
   end
 
   def test_marshalling
@@ -41,6 +47,13 @@ class QueryTest < Test::Unit::TestCase
     m = Marshal.dump(min)
     mout = Marshal.load m
     assert_equal Set.new, mout.matches(%w(some text that can't match))
+  end
+
+  private
+
+  def run_test(t, &block)
+    yield t
+    yield Marshal.load(Marshal.dump(t))
   end
 
 end
