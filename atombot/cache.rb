8616537179d1acebc8c2103db977f1d441f37c5e
Dustin Sallings <dustin@spy.net>
2008-10-06 22:09:41 -0700

    Track version numbers and cache numbers together.

--- atombot/cache.rb
+++ atombot/cache.rb
@@ -6,7 +6,8 @@ require 'atombot/config'
 module AtomBot
   module Cache
 
-    MATCH_KEY = "matches-trie" unless defined? MATCH_KEY
+    CACHE_VERSION=5
+    MATCH_KEY = "matches-#{CACHE_VERSION}" unless defined? MATCH_KEY
 
     def init_cache
       conf = AtomBot::Config::CONF['general']
@@ -19,12 +20,12 @@ module AtomBot
       @cache ||= init_cache
     end
 
-    def get_version_num(name='version')
+    def get_version_num(name="version-#{CACHE_VERSION}")
       cache.add(name, "0")
       cache.incr(name, 0)
     end
 
-    def new_version_num(name='version')
+    def new_version_num(name="version-#{CACHE_VERSION}")
       cache.add(name, "0")
       cache.incr(name)
     end
