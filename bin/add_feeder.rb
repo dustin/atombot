0723077e789ed54535716d069bfc8c9185e80135
Dustin Sallings <dustin@spy.net>
2008-11-10 19:32:06 -0800

    Fix ridiculous quote problem.

--- bin/add_feeder.rb
+++ bin/add_feeder.rb
@@ -15,8 +15,8 @@ raise "Usage:  #{$0} shortname baseurl" if url.host.nil?
 s = Service.new :name => name, :hostname => url.host,
   :api_path => url.path + "api",
   :jid => jid,
-  :user_pattern => %Q{<a href="#{url.to_s}#\{user\}">#\{linktext\}</a>"},
-  :tag_pattern => %Q{<a href="#{url.to_s}tag/#\{tag\}">#\{linktext\}</a>"},
+  :user_pattern => %Q{<a href="#{url.to_s}#\{user\}">#\{linktext\}</a>},
+  :tag_pattern => %Q{<a href="#{url.to_s}tag/#\{tag\}">#\{linktext\}</a>},
   :listed => false, :api_key => WEBrick::Utils.random_string(12)
 
 s.save
